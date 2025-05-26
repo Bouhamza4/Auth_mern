6b58b09b904c2e319ab10e5a8bb7dbfeb6ba72e6
carcacheoussama <carcacheoussama@gmail.com>
2025-05-26 02:09:45 +0100

    update password and get user and routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { register, login, getAllUsers } from "../controllers/authController.js";
+import { register, login, getAllUsers , getUserById , updatePassword } from "../controllers/authController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
-
+//bouhamza
 router.post("/register", register);
 router.post("/login", login);
 router.get("/users", authMiddleware, getAllUsers);
+// Oussama
+router.get("/users/:id", authMiddleware, getUserById);
+router.put("/users/:id/password", authMiddleware, updatePassword);
 
 export default router;
